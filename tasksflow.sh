0c1eb618c851934c59027a0f6b7b6534fc89c57e
ljmotley <ljmotley@gmail.com>
2022-05-22 18:14:21 -0500

    cleaning up more

--- tasksflow.sh
+++ tasksflow.sh
@@ -1,22 +1,22 @@
 if [ "$#" -eq  "0" ]
     then
-      echo -e 'digraph G {' > taskflow.txt #Start graph
+      echo -e 'digraph G {' > tasksflow.txt #Start graph
       find tasks -type l -ls | awk '{print $13 " -> " $11}' | #List symbolic links
       sed 's/\.\.\///g' | sed 's/tasks\///g' | #Drop relative paths
       sed 's/\/\(input\)\/[a-zA-Z0-9_\.]*//g' | #Retain only task names; drop filenames
       sed 's/\/\(temp\)\/[a-zA-Z0-9_\.]*//g' | #Retain only task names; drop filenames
-      sed 's/\/\(output\)\/[a-zA-Z0-9_\.]*//g' >> taskflow.txt
-      echo '}' >> taskflow.txt
+      sed 's/\/\(output\)\/[a-zA-Z0-9_\.]*//g' >> tasksflow.txt
+      echo '}' >> tasksflow.txt
     else
-      echo -e 'digraph G {' > taskflow.txt #Start graph
+      echo -e 'digraph G {' > tasksflow.txt #Start graph
       find tasks -type l -ls | awk '{print $13 " -> " $11}' | #List symbolic links
       sed 's/\.\.\///g' | sed 's/tasks\///g' | #Drop relative paths
       sed 's/\/\(input\)\/[a-zA-Z0-9_\.]*//g' | #Retain only task names; drop filenames
       sed 's/\/\(temp\)\/[a-zA-Z0-9_\.]*//g' | #Retain only task names; drop filenames
       sed 's/\/\(output\)\/[a-zA-Z0-9_\.]*//g' |
-      sed '/'$1'/ s/$/ [color =blue, fillcolor = blue]/' >> taskflow.txt
-      echo ''$1' [color = blue, fontcolor = blue]' >> taskflow.txt
-      echo '}' >> taskflow.txt
+      sed '/'$1'/ s/$/ [color =blue, fillcolor = blue]/' >> tasksflow.txt
+      echo ''$1' [color = blue, fontcolor = blue]' >> tasksflow.txt
+      echo '}' >> tasksflow.txt
 fi
-dot -Grankdir=LR -Tpng taskflow.txt -o taskflow.png
-rm taskflow.txt
+dot -Grankdir=LR -Tpng tasksflow.txt -o tasksflow.png
+rm tasksflow.txt
